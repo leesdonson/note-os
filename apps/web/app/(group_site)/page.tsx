aaa5c628b1f5c259d3a31777cba43832a910cded
Lee Donson <leesdev44@gmail.com>
2026-04-29 23:35:32 +1000

    Setup TailwindCSS

---
+++ apps/web/app/(group_site)/page.tsx
@@ -0,0 +1,9 @@
+import { HeroSection } from "@/components/pages/hero";
+
+export default function MainPage() {
+  return (
+    <div className="flex flex-col h-screen items-center justify-center bg-black text-white">
+      <HeroSection />
+    </div>
+  );
+}
---
+++ apps/web/components/pages/hero.tsx
@@ -0,0 +1,14 @@
+export function HeroSection() {
+  return (
+    <div className="flex w-full flex-col h-screen items-center justify-center bg-black text-white">
+      <div className="p-5 rounded-2xl max-w-2xl mx-auto w-full border border-red-600">
+        <h1>Hero Page</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Reprehenderit ducimus sed enim, iusto unde perspiciatis numquam
+          sapiente nostrum dicta suscipit.
+        </p>
+      </div>
+    </div>
+  );
+}
